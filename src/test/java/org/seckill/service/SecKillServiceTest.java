d35014a23ab8a22f6833400e5c90ec88c1ee71a3
tkst1993 <931559905@qq.com>
2016-06-30 13:14:04 +0800

    测试小改动

--- src/test/java/org/seckill/service/SecKillServiceTest.java
+++ src/test/java/org/seckill/service/SecKillServiceTest.java
@@ -38,13 +38,13 @@ public class SecKillServiceTest {
 
     @Test
     public void testExportSeckillUrl() throws Exception {
-        System.out.println("test");
+
 
     }
 
     @Test
     public void testExecuteSecKill() throws Exception {
-        System.out.println("test2");
+
 
     }
 }
